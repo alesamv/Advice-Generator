5a3f648fa1f4ef0009832768e3b26936fe099565
alesamv <alesamv@gmail.com>
2022-08-10 16:27:22 -0500

    finished project

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
 
 const API = "https://api.adviceslip.com/advice";
 
-let advice = document.getElementById("advice");
+const adviceNumber = document.getElementById("number__advice");
+const adviceText = document.getElementById("text__advice");
+const btn = document.getElementById("refresh");
+
+btn.addEventListener('click', btnRefresh)
 
 async function fetchData(urlApi) {
     const response = await fetch(urlApi);
     const data = await response.json();
 
-    advice.innerHTML = `
-        <h6 class="text-[#51FFA8]">A D V I C E # ${data.slip.id} </h6>
-        <p class="text-[#CDE2E8] text-[28px] font-[800]">"${data.slip.advice}" </p>
-    `
+    adviceNumber.innerHTML = `ADVICE #${data.slip.id}`;
+    adviceText.innerHTML = `"${data.slip.advice}"`;
+}
+
+function btnRefresh() {
+    fetchData(API);
 }
 
 fetchData(API);
